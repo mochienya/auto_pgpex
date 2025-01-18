b2af1522ae24e7811bdc973124a3ccff3be51e10
cassie <fseusb@gmail.com>
2025-01-17 17:25:01 -0700

    improve logging

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,7 @@ export default antfu({
   rules: {
     'antfu/no-top-level-await': 'off',
     'no-console': 'off',
+    'style/max-statements-per-line': 'off',
+    'node/prefer-global/process': 'off',
   },
 })
--- index.ts
+++ index.ts
@@ -5,14 +5,18 @@ import * as openpgp from 'openpgp'
 const armoredMessage = clipboard.readSync()
 
 const message = await openpgp.readMessage({ armoredMessage })
-  .catch(() => { throw new Error('Invalid PGP Message in clipboard') })
+  .catch(() => { console.error('Invalid PGP Message in clipboard'); process.exit() })
 
-const armoredKey = await file('./private_key.asc').text()
+const armoredKey = await file('./private_key.asc')
+  .text()
+  .catch(() => { console.error('Failed to read private_key.asc, does it exist?'); process.exit() })
 const decryptionKeys = await openpgp.readPrivateKey({ armoredKey })
-  .catch(() => { throw new Error('Invalid PGP Private Key') })
+  .catch(() => { console.error('Invalid PGP Private Key'); process.exit() })
 
 const { data: decryptedMessage } = await openpgp.decrypt({ message, decryptionKeys })
+  .catch(() => { console.error('PGP Decryption Error'); process.exit() })
 
 const key = /[0-9a-f]{56}/.exec(decryptedMessage)![0]
 
 clipboard.writeSync(key)
+console.log('\u001B[32mKey copied to clipboard!\u001B[0m')
