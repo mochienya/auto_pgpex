1ddb42b1a4e9d76bf93f26ea3b029c5625bd3d12
mochie <mop48duck+cassie@gmail.com>
2025-02-17 04:51:32 +0100

    r: add delay before exit

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 	"regexp"
 
 	"github.com/ProtonMail/gopenpgp/v3/crypto"
@@ -12,25 +13,33 @@ import (
 
 func main() {
 	keyFile, err := os.Open("private_key.asc")
-	if err != nil { log.Fatal("Failed to read private_key.asc, does it exist?") }
+	if err != nil { errHandle("Failed to read private_key.asc, does it exist?") }
 	defer keyFile.Close()
 
 	clip := clipboard.New()
 	pgp := crypto.PGP()
 
 	clipContents, err := clip.PasteText()
-	if err != nil { log.Fatal("Failed to read clipboard") }
+	if err != nil { log.Panic("Failed to read clipboard") }
 
 	privKey, err := crypto.NewKeyFromReader(keyFile)
-	if err != nil { log.Fatal("Invalid key in file private_key.asc") }
+	if err != nil { errHandle("Invalid key in file private_key.asc") }
 	decHandler, err := pgp.Decryption().DecryptionKey(privKey).New()
-	if err != nil { log.Fatal(err) }
+	if err != nil { errHandle("yeah this wasn't my fault (i think). something went really fucking wrong") }
 	decrypted, err := decHandler.Decrypt([]byte(clipContents), crypto.Auto)
-	if err != nil { log.Fatal("Decryption failed, is the message in your clipboard correct?") }
+	if err != nil { errHandle("Decryption failed, is the message in your clipboard correct?") }
 
 	regex := regexp.MustCompile(`[0-9a-f]{56}`)
 	final := regex.FindString(string(decrypted.Bytes()[:]))
 
 	clip.CopyText(final)
-	fmt.Println("\u001B[32mKey copied to clipboard!\u001B[0m")
+	fmt.Println("\033[32mKey copied to clipboard!\033[0m")
+	time.Sleep(2 * time.Second)
+	os.Exit(0)
+}
+
+func errHandle(message string) {
+	fmt.Println("\033[31mError: ", message, "\033[0m")
+	time.Sleep(3 * time.Second)
+	os.Exit(1)
 }
